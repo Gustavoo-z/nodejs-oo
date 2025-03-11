0cdd13486b422ce1b0471bc7a505fd4ee9d8f188
Gustavo Zesczylinski <germangz@rocketmail.com>
2025-03-11 16:12:42 -0300

    Add object

---
+++ objetoLiteral.js
@@ -0,0 +1,23 @@
+const user = {
+    nome: 'Juliana',
+    email: 'j@j.com',
+    nascimento: '2024-01-01',
+    role: 'estudante',
+    ativo: true,
+    exibirInfos: function() {
+        console.log(this.nome, this.email);
+    }
+}
+
+user.exibirInfos();
+
+const exibir = user.exibirInfos;
+exibir(); // undefined undefined
+
+const exibir1 = function() {
+    console.log(this.nome, this.email);
+}
+exibir1(); // undefined undefined
+
+const exibirNome = exibir1.bind(user);
+exibirNome(); 
